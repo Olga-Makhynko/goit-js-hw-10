b13efb1836c4b8c947d93818c9ed4fef2d435090
Olga <olichka7891olichka@gmail.com>
2025-04-16 20:24:22 +0100

    made changes in html

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,7 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const startButton = document.querySelector('[data-start]');
 
